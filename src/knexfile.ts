e917524aee43a9b7b93e2a3966688f79c8117f4b
jschneiderm98 <julio_schneider@hotmail.com>
2021-08-06 17:55:46 -0300

    deploy

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -65,7 +65,7 @@ export const test = {
 };
 export const production = {
   client: 'postgresql',
-  connection: process.env.DATABASE_URL || '',
+  connection: (process.env.DATABASE_URL || '')+'?ssl=true',
   pool: {
     min: 2,
     max: 10,
